92ee337b25536fc3b670dbff963e2cfa46d6b0d8
EGOISTK <chinazhjjq@qq.com>
2015-10-09 15:15:28 +0800

    DEMO

--- app/src/main/java/com/example/egoistk/infinite_color/MainActivity.java
+++ app/src/main/java/com/example/egoistk/infinite_color/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.egoistk.infinite_color;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);//隐藏标题栏
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//隐藏状态栏
         setContentView(R.layout.activity_main);
     }
 }
