456b77e3d2ba427c29aec1d04cac90c87b31c885
EGOIST_K <chinazhjjq@qq.com>
2015-10-14 23:22:41 +0800

    新新的三个布局

--- app/src/main/java/com/example/egoistk/infinite_color/MainActivity.java
+++ app/src/main/java/com/example/egoistk/infinite_color/MainActivity.java
@@ -1,17 +1,16 @@
 package com.example.egoistk.infinite_color;
 
-import android.app.ActionBar;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Layout;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.FrameLayout;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    private FrameLayout root;
-    private MainLaunchView mainLaunchView;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btn_gallery, btn_about;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,26 +18,24 @@ public class MainActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//隐藏状态栏
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.btn_infinite).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, InfiniteActivity.class));
-            }
-        });
+        btn_gallery  = (Button) findViewById(R.id.btn_gallery);
+        btn_about    = (Button) findViewById(R.id.btn_about);
+        btn_gallery.setOnClickListener(this);
+        btn_about.setOnClickListener(this);
 
-        findViewById(R.id.btn_color).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, ColorActivity.class));
-            }
-        });
 
-        findViewById(R.id.btn_about).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_gallery:
+                startActivity(new Intent(MainActivity.this, GalleryActivity.class));
+                break;
+            case R.id.btn_about:
                 startActivity(new Intent(MainActivity.this, AboutActivity.class));
-            }
-        });
+                break;
 
+        }
     }
 }
